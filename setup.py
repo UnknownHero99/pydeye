0887be43189ec0e02d1496b37a115622364174e1
UnknownHero99 <salmic.jakob@gmail.com>
2025-08-09 00:02:43 +0200

    0.0.8 minor fix

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="pydeye",
-    version="0.0.7",
+    version="0.0.8",
     description="Python deye inverter interface",
     long_description=long_description,
     long_description_content_type="text/markdown",

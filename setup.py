aa265be6ea12030f95252b2e11827453b09f9a62
UnknownHero99 <salmic.jakob@gmail.com>
2025-08-08 23:53:22 +0200

    0.0.7 release

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="pydeye",
-    version="0.0.6",
+    version="0.0.7",
     description="Python deye inverter interface",
     long_description=long_description,
     long_description_content_type="text/markdown",
